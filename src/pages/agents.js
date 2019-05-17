574fca730a4459ee26463326388cef400f80a19b
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-05-17 22:00:44 +0800

    added front page

---
+++ src/pages/agents.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+import Nav from '../components/nav'
+import Footer from '../components/footer'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+class Agents extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Agents
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,52 +17,11 @@ class Home extends React.Component {
       <main role="main">
         <Nav />
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-            <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-          </ol>
-          <div class="carousel-inner">
-            <div class="carousel-item">
-              <img class="first-slide" src="" alt="First slide"></img>
-              <div class="container">
-                <div class="carousel-caption text-left">
-                  <h1>Example headline.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Sign up today</a></p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item active">
-              <img class="second-slide" src="" alt="Second slide"></img>
-              <div class="container">
-                <div class="carousel-caption">
-                  <h1>Another example headline.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Learn more</a></p>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img class="third-slide" src="" alt="Third slide"></img>
-              <div class="container">
-                <div class="carousel-caption text-right">
-                  <h1>One more for good measure.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Browse gallery</a></p>
-                </div>
-              </div>
-            </div>
+        <div class="jumbotron bg-cover main-cover">
+          <div class="container">
+            <h1 class="display-3">Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
           </div>
-          <a class="carousel-control-prev" href="https://getbootstrap.com/docs/4.0/examples/carousel/#myCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="https://getbootstrap.com/docs/4.0/examples/carousel/#myCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
 
         <div class="container marketing">
@@ -72,19 +31,19 @@ class Home extends React.Component {
               <img class="rounded-circle" src="./package.jpg" alt="Generic placeholder image" width="140" height="140"></img>
               <h2>Comprehensive Process</h2>
               <p>Our sourcing process provides a reliable solution for all types of importers.</p>
-              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+              <p><a class="btn btn-secondary" href="./process" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
               <img class="rounded-circle" src="./agent.jpg" alt="Generic placeholder image" width="140" height="140"></img>
               <h2>Professional<br /> Agents</h2>
-              <p>Our team of agents can help you to get a quotation within 48 hours.</p>
-              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+              <p>Our team of professional agents can help you to get a quotation within 48 hours.</p>
+              <p><a class="btn btn-secondary" href="./agents" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
               <img class="rounded-circle" src="./deal.jpg" alt="Generic placeholder image" width="140" height="140"></img>
               <h2>Competitive<br /> Rates</h2>
               <p>We charge fair rates through a transparent sourcing process perfect for small to medium-sized businesses.</p>
-              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+              <p><a class="btn btn-secondary" href="./pricing" role="button">View details »</a></p>
             </div>
           </div>
 
@@ -94,11 +53,12 @@ class Home extends React.Component {
 
           <div class="row featurette">
             <div class="col-md-7">
-              <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              <h2 class="featurette-heading">Get your first quote today. <span class="text-muted">It's free.</span></h2>
+              <p class="lead">We'll help you get a quotation from suppliers of your product for free within 48 hours.</p>
+              <p><a class="btn btn-lg btn-primary" href="./contact" role="button">Contact Us</a></p>
             </div>
             <div class="col-md-5">
-              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="" data-holder-rendered="true"></img>
+              <img class="featurette-image img-fluid mx-auto" alt="computer" src="./computer.jpg"></img>
             </div>
           </div>
 
@@ -106,11 +66,12 @@ class Home extends React.Component {
 
           <div class="row featurette">
             <div class="col-md-7 order-md-2">
-              <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              <h2 class="featurette-heading">Your supply chain.<br /><span class="text-muted">In our hands.</span></h2>
+              <p class="lead">Let us take care of your sourcing and management for your entire product today</p>
+              <p><a class="btn btn-lg btn-primary" href="./process" role="button">Learn More</a></p>
             </div>
             <div class="col-md-5 order-md-1">
-              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="" data-holder-rendered="true" ></img>
+              <img class="featurette-image img-fluid mx-auto" alt="warehouse" src="./warehouse.jpg" ></img>
             </div>
           </div>
 
