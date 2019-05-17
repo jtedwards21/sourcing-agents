9fa56ec76427e5c0bf3b5209222ee7958a051043
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-05-17 21:25:40 +0800

    front page pics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+import "./src/styles/main.css"
+
+import "./src/styles/carousel.css"
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,43 +5,131 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import Nav from "../components/nav"
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+
+
 
 
 class Home extends React.Component {
   render() {
     return (
-      <body class="text-center">
+      <main role="main">
+        <Nav />
 
-        <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-          <header class="masthead mb-auto">
-            <div class="inner">
-              <h3 class="masthead-brand">Cover</h3>
-              <nav class="nav nav-masthead justify-content-center">
-                <a class="nav-link active" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Contact</a>
-              </nav>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+            <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img class="first-slide" src="" alt="First slide"></img>
+              <div class="container">
+                <div class="carousel-caption text-left">
+                  <h1>Example headline.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Sign up today</a></p>
+                </div>
+              </div>
             </div>
-          </header>
-
-          <main role="main" class="inner cover">
-            <h1 class="cover-heading">Cover your page.</h1>
-            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-            </p>
-          </main>
-
-          <footer class="mastfoot mt-auto">
-            <div class="inner">
-              <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            <div class="carousel-item active">
+              <img class="second-slide" src="" alt="Second slide"></img>
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Another example headline.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Learn more</a></p>
+                </div>
+              </div>
             </div>
-          </footer>
+            <div class="carousel-item">
+              <img class="third-slide" src="" alt="Third slide"></img>
+              <div class="container">
+                <div class="carousel-caption text-right">
+                  <h1>One more for good measure.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">Browse gallery</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="https://getbootstrap.com/docs/4.0/examples/carousel/#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="https://getbootstrap.com/docs/4.0/examples/carousel/#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
-      </body>
+
+        <div class="container marketing">
+
+          <div class="row">
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="./package.jpg" alt="Generic placeholder image" width="140" height="140"></img>
+              <h2>Comprehensive Process</h2>
+              <p>Our sourcing process provides a reliable solution for all types of importers.</p>
+              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+            </div>
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="./agent.jpg" alt="Generic placeholder image" width="140" height="140"></img>
+              <h2>Professional<br /> Agents</h2>
+              <p>Our team of agents can help you to get a quotation within 48 hours.</p>
+              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+            </div>
+            <div class="col-lg-4">
+              <img class="rounded-circle" src="./deal.jpg" alt="Generic placeholder image" width="140" height="140"></img>
+              <h2>Competitive<br /> Rates</h2>
+              <p>We charge fair rates through a transparent sourcing process perfect for small to medium-sized businesses.</p>
+              <p><a class="btn btn-secondary" href="https://getbootstrap.com/docs/4.0/examples/carousel/#" role="button">View details »</a></p>
+            </div>
+          </div>
+
+
+
+          <hr class="featurette-divider"></hr>
+
+          <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="" data-holder-rendered="true"></img>
+            </div>
+          </div>
+
+          <hr class="featurette-divider"></hr>
+
+          <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+              <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5 order-md-1">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="" data-holder-rendered="true" ></img>
+            </div>
+          </div>
+
+          <hr class="featurette-divider"></hr>
+
+          <div class="row featurette">
+            <div class="col-md-7">
+              <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+              <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </div>
+            <div class="col-md-5">
+              <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="" data-holder-rendered="true"></img>
+            </div>
+          </div>
+
+          <hr class="featurette-divider"></hr>
+
+        </div>
+        </main>
     )
   }
 }
